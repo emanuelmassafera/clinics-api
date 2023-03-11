89ff5a171e53530ce2f51e69bcf15eaf33806729
Emanuel Massafera <emanuel301@live.com>
2023-03-11 12:34:12 -0300

    feat: add http helper

---
+++ src/presentation/controllers/helpers/http-helper.ts
@@ -0,0 +1,11 @@
+import { Http } from '../../protocols/http';
+
+export const ok = <T extends any>(data: T): Http.Response<T> => ({
+  statusCode: 200,
+  body: data,
+});
+
+export const noContent = (): Http.Response<null> => ({
+  statusCode: 204,
+  body: null,
+});
