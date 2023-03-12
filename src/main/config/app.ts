dca53ff943cfa3959ce5ce5af20b9fac9253d548
Emanuel Massafera <emanuel301@live.com>
2023-03-12 15:26:28 -0300

    feat: add swagger plugin

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify';
+import swaggerPlugin from './swagger';
 import clinicsRoutes from '../routes/clinics';
 
 const app = fastify();
 
 app.register(clinicsRoutes);
+
+app.register(swaggerPlugin);
 
 export default app;
---
+++ src/main/config/swagger.ts
@@ -0,0 +1,27 @@
+import fp from 'fastify-plugin';
+import { FastifyPluginAsync } from 'fastify';
+import fastifySwagger, { FastifyDynamicSwaggerOptions } from '@fastify/swagger';
+import fastifySwaggerUi, { FastifySwaggerUiOptions } from '@fastify/swagger-ui';
+
+const swaggerPlugin: FastifyPluginAsync = async (app) => {
+  const openApiOptions: FastifyDynamicSwaggerOptions = {
+    openapi: {
+      info: {
+        title: 'Clinics API',
+        description: 'RESTful API that allows searching across multiple clinic providers',
+        version: '1.0.0',
+      },
+    },
+    hideUntagged: true,
+  };
+
+  await app.register(fastifySwagger, openApiOptions);
+
+  const openApiUiOptions: FastifySwaggerUiOptions = {
+    routePrefix: '/api-docs',
+  };
+
+  await app.register(fastifySwaggerUi, openApiUiOptions);
+};
+
+export default fp(swaggerPlugin);
