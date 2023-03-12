d0644e662b2def19a17a07aa84294e8d0bda3159
Emanuel Massafera <emanuel301@live.com>
2023-03-12 09:09:09 -0300

    refactor: improve route adapter

--- src/main/adapters/fastify-route-adapter.ts
+++ src/main/adapters/fastify-route-adapter.ts
@@ -1,11 +1,17 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { Http } from '../../presentation/protocols/http';
 
-export default (controller: Http.Controller) => async (
+const adaptRoute = (controller: Http.Controller) => async (
   request: FastifyRequest,
   reply: FastifyReply,
 ) => {
-  const httpResponse = await controller.handle(request);
+  const httpResponse = await controller.handle({
+    ...(request.body || {}),
+    ...(request.params || {}),
+    ...(request.query || {}),
+  });
 
   reply.status(httpResponse.statusCode).send(httpResponse.body);
 };
+
+export default adaptRoute;
